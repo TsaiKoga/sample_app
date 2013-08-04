087225291d53798caab1f9d2fc3f5478e395b41b
TsaiKoga <969024089@qq.com>
2013-08-04 14:45:08 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   get "users/new"
   # get "static_pages/help"
-	get 'users/new'
+  match '/signup', to: "users#new", via: 'get'
   match '/help', to: "static_pages#help", via: 'get'
   match '/about', to: "static_pages#about", via: 'get'
 	match '/contact', to: "static_pages#contact", via: 'get'
