f02a692302f88ff7a95ac94ec45a68d8dc45351e
CKJ <wando@wando.(none)>
2013-08-05 22:23:16 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-    if @user.save
-    else
-      render "new"
-    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   private
